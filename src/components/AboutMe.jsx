1f9e130815971de211153edfa51871e0ff56c207
Jose Torres <vidojam@gmail.com>
2023-04-19 12:53:58 -0400

    first commit

---
+++ src/components/AboutMe.jsx
@@ -0,0 +1,81 @@
+
+export default function AboutMe(){
+  return(
+    <>
+      <h1>About Me</h1>
+      <section className="container">
+
+      </section>
+      <h2>Favorite Quote</h2>
+      <p>Motivation is what gets you started. Habit is what keeps you going. - Jim Rohn</p>
+
+      <h2>Who Am I?</h2>
+      <p>Life, what a incredible Journey. I am always seeking self-knowledge and personal growth.<br/><br/>Ten strikes in a row, for a <strong>289 bowling score!</strong> Gotta give it another shot!<br/> <br/> </p>
+
+
+    </>
+  )
+}   
+
+// Image- A professional Image of yourself(you will have the opportunity to update the photo once we take head shots)
+// Quote- A personal mantra or favorite quote
+// Who You Are- A brief paragraph describing who you are, including a fun fact, your background, and your goals after completing the course(no more than 3-4 sentences).
+// Why BC- An explanation of why you chose Boca Code, and what motivated you to pursue a career in software engineering.
+// Projects- Now that you have a new superpower, what is one project/idea you are looking forward to build? :female_superhero::male_superhero:
+// Ideal Workplace- What  qualities do you look for in your ideal workplace or dream company(culture etc)?
+// Please note that your “About Me” page MUST be a React app, using React components and a minimum of 2-3 React hooks.
+// Due Date:
+// All assignment links must be posted on slack no later than Friday April 21, 2023 at 9:00am. Any assignment submitted AFTER 9:00am will NOT be accepted. (Due Before class on Friday)
+// The project MUST be deployed on Firebase--- Local host will not be accepted.
+  
+
+// Why BC- An explanation of why you chose Boca Code, and what motivated you to pursue a career in software engineering.
+// RETURNING TO A PREVIOUS LOVE, CODING, SEEMED LIKE JUST THE RIGHT THING TO DO IN RETIREMENT FOR 5, 10, 15…? YEARS!!!
+
+
+
+// Projects- Now that you have a new superpower, what is one project/idea you are looking forward to build? 
+
+// MY PROJECT IDEA FOR THE MOMENT IS IN THE AREA OF STOCKS, AND HOW WE CAN HAVE AN ADDITIONAL TOOL TO MAKE THOSE BUYING DECISIONS
+
+
+
+// :female_superhero::male_superhero:
+// Ideal Workplace- What  qualities do you look for in your ideal workplace or dream company(culture etc)?
+// BOCA CODE IS A GREAT EXAMPLE OF AN IDEAL WORKPLACE. EVERY WEEK FINISHES WITH THINGS GETTING DONE, ALMOST WITHOUT MANAGEMENT. EACH PERSON WORKS TOWARDS DOING THERE BEST FOR THEMSELVES AND THE ORGANIZATION. 
+
+// Please note that your “About Me” page MUST be a React app, using React components and a minimum of 2-3 React hooks.
+
+
+
+
+
+
+
+
+
+// export default function Counter() {
+//   const [count, setCount] = useState(0)
+//   return (
+//     <>
+//     <h1>Counter: {count}</h1>
+//     <button onClick={() => setCount(count + 1)}>+1</button>
+//     </>
+//   )
+// }
+
+// import Counter from './components/Counter';
+
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <Counter />
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
